5547d66fe7c80a31cdabfc801fa4145090ccc71c
phayusa <sokomo873@free.fr>
2017-03-22 10:34:41 +0100

    Modification of factory

--- services/mainService.js
+++ services/mainService.js
@@ -1,22 +1,21 @@
 app.factory('clientFactory', function($http){
   var factory = {
-    listClient : [
-      {
-        nom : 'Dupont',
-        prenom : 'Jean',
-        mail : 'jean@dupont.fr'
-      }, {
-        nom : 'Durand',
-        prenom : 'Christine',
-        mail : 'christine@durand.fr'
-      },
-    ],
     getAllClient: function() {
       return $http.get('http://localhost:8888/clients');
     },
     getClient: function(id) {
       return $http.get('http://localhost:8888/client/'+id);
+    },
+    addClient: function(client){
+      return $http.post('http://localhost:8888/client/',client);
+    },
+    deleteClient: function(id){
+      return $http.delete('http://localhost:8888/client/'+id);
+    },
+    modifyClient : function(id, clientUpdate){
+      return $http.put('http://localhost:8888/client/'+id,clientUpdate);
     }
+
   }
 
   return factory;
