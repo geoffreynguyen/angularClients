e6e571864212d03b00680f38d3841c7b4c16c944
phayusa <sokomo873@free.fr>
2017-03-22 15:18:59 +0100

    Add of services for folderService

---
+++ services/folderService.js
@@ -0,0 +1,22 @@
+app.factory('folderFactory', function($http){
+  var factory = {
+    getAllFolder: function() {
+      return $http.get('http://localhost:8888/folders');
+    },
+    getFolder: function(id) {
+      return $http.get('http://localhost:8888/folder/'+id);
+    },
+    addFolder: function(folder){
+      return $http.post('http://localhost:8888/folders/',folder);
+    },
+    deleteFolder: function(id){
+      return $http.delete('http://localhost:8888/folder/'+id);
+    },
+    modifyFolder : function(id, folderUpdate){
+      return $http.put('http://localhost:8888/folder/'+id,folderUpdate);
+    }
+
+  }
+
+  return factory;
+});
