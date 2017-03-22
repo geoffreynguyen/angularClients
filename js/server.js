615460934fa7780b330b925a911a5d036e99cf62
phayusa <sokomo873@free.fr>
2017-03-22 09:59:38 +0100

    Finish CRUD of clients

--- js/server.js
+++ js/server.js
@@ -4,44 +4,103 @@ var ObjectID = require('mongodb').ObjectID;
 var MongoClient = require("mongodb").MongoClient;
 var bodyParser = require('body-parser');
 
+var dbName = "mongodb://localhost/Clients";
+var clientTable = "clients";
+
 // support json encoded bodies
 app.use(bodyParser.json());
 // support encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/clients',function(req,res){
-  console.log("receive port 8888 /clients");
-  MongoClient.connect("mongodb://localhost/Clients", function(error, db) {
-      if (error) throw error;
-
-      db.collection("clients").find().toArray(function (error, results) {
-        if (error) throw error;
-        res.json(results);
-      });
-  });
-})
-
-app.get('/clients/:id',function(req,res){
-  console.log("receive port 8888 /clients/"+req.params.id);
-  MongoClient.connect("mongodb://localhost/Clients", function(error, db) {
-      if (error) throw error;
-      var o_id = new ObjectID(req.params.id);
-
-      // console.log("ObjectId(\""+req.params.id+"\")");
-      db.collection("clients").find({"_id":o_id}).toArray(function (error, results) {
-        if (error) throw error;
-        res.json(results);
-      });
-  });
-})
-
-app.post('/client/')
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+
+app.get('/clients',function(req,res){
+  console.log("receive port 8888 /clients");
+  MongoClient.connect(dbName, function(error, db) {
+      if (error) throw error;
+
+      db.collection(clientTable).find().toArray(function (error, results) {
+        if (error) throw error;
+        res.json(results);
+      });
+
+      db.close();
+  });
+})
+
+app.get('/clients/:id',function(req,res){
+  console.log("receive port 8888 /clients/"+req.params.id);
+  MongoClient.connect(dbName, function(error, db) {
+      if (error) throw error;
+      var o_id = new ObjectID(req.params.id);
+
+      // console.log("ObjectId(\""+req.params.id+"\")");
+      db.collection(clientTable).find({"_id":o_id}).toArray(function (error, results) {
+        if (error) throw error;
+        res.json(results);
+      });
+
+      db.close();
+
+  });
+})
+
+app.post('/clients/', function(req, res) {
+    var name = req.body.nom;
+    var surname = req.body.prenom;
+
+    MongoClient.connect(dbName, function(error, db) {
+        if (error) throw error;
+
+        db.collection(clientTable).insert(req.body, function (error, results) {
+          if (error) throw error;
+          // use insertedCount to know how many is insert
+          res.json(results);
+        });
+
+        db.close();
+
+    });
+});
+
+app.delete('/client/:id',function(req,res){
+
+  MongoClient.connect(dbName, function(error, db) {
+      if (error) throw error;
+      var o_id = new ObjectID(req.params.id);
+
+      // use n to know it was delete
+      db.collection(clientTable).deleteOne({"_id":o_id}, null, function (error, results) {
+        if (error) throw error;
+        res.json(results);
+      });
+
+      db.close();
+
+  });
+})
+
+app.put('/client/:id',function(req,res){
+  // res.send(req.params.id + " "+req.body.nom+" "+req.body.prenom);
+  MongoClient.connect(dbName, function(error, db) {
+      if (error) throw error;
+      var o_id = new ObjectID(req.params.id);
+
+      console.log("Before update");
+      // use n to know it was delete
+      db.collection(clientTable).update({"_id":o_id},{$set:{"nom":req.body.nom,"prenom":req.body.prenom}});
+      console.log("After update");
+      res.send("ok");
+      db.close();
+
+  });
+})
 
 
 app.listen(8888,function(){
